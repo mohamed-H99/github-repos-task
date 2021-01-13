f7f3e8db78fd07947be72aaf1c3dd574acde966a
ahmed hashem <ahmdbhgat4@gmail.com>
2021-01-13 16:19:54 +0200

    remove unused icons

--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -1,7 +1,3 @@
-import 'vue-awesome/icons/arrow-alt-circle-left'
-import 'vue-awesome/icons/arrow-alt-circle-right'
-import 'vue-awesome/icons/times'
-import 'vue-awesome/icons/home'
 import 'vue-awesome/icons/poll'
 import 'vue-awesome/icons/search'
 import 'vue-awesome/icons/star'
